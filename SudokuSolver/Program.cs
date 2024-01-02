e2b88f0d63e10786ce195d98d804e8fdd7af7317
rimeger <qfarmir@gmail.com>
2024-01-02 16:55:03 +0300

    Add project files.

---
+++ SudokuSolver/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SudokuSolver
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solver solver = new Solver(
+                new char[,] {
+                { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
+                { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+                { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+                { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+                { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+                { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+                { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+                { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
+                { '.', '.', '.', '.', '8', '.', '.', '7', '9' }
+                }
+                );
+            solver.PrintSudoku();
+            if (solver.SolveSudoku()) {
+                Console.WriteLine("\nSolved One:\n");
+                solver.PrintSudoku();
+            }
+            else {
+                Console.WriteLine("\nThere is no solution...");
+            }
+        }
+    }
+}
---
+++ SudokuSolver/Solver.cs
@@ -0,0 +1,72 @@
+﻿namespace SudokuSolver
+{
+    public class Solver
+    {
+        private char[,] _board;
+        private int _size;
+
+        public Solver(char[,] board)
+        {
+            _board = board;
+            _size = board.GetLength(0);
+        }
+
+        public bool SolveSudoku()
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = 0; j < _size; j++)
+                {
+                    if (_board[i, j] == '.')
+                    {
+                        for (char k = '1'; k <= Convert.ToChar(_size + '0'); k++)
+                        {
+                            if(IsPossible(i, j, k))
+                            {
+                                _board[i, j] = k;
+                                if (SolveSudoku()) {
+                                    return true;
+                                }
+                                else
+                                {
+                                    _board[i, j] = '.';
+                                }
+                            }
+                        }
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private bool IsPossible(int row, int col, char number)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                if (_board[row, i] == number) {
+                    return false;
+                }
+                if (_board[i, col] == number) { 
+                return false;
+                }
+                if (_board[3 * (row / 3) + i / 3, 3 * (col / 3) + i % 3] == number) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void PrintSudoku()
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = 0; j < _size; j++)
+                {
+                    Console.Write(_board[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
